a3a354ac65839458a73ad6ae926b6a030bee3ebc
Donnahue George <Donnahuegjr@gmail.com>
2021-05-26 10:11:22 -0400

    added temporary host file and writing information to the temp host file

--- blocker.py
+++ blocker.py
@@ -1,6 +1,7 @@
 import time
 from datetime import datetime as dt
 
+host_temp = "hosts"
 hosts_path_Windows = "C:\Windows\System32\drivers\etc\hosts"
 host_path_Mac = "/etc/hosts"
 redirect = "127.0.0.1"
@@ -10,9 +11,16 @@ while True:
     if (
         dt(dt.now().year, dt.now().month, dt.now().day, 8)
         < dt.now()
-        < dt(dt.now().year, dt.now().month, dt.now().day, 16)
+        < dt(dt.now().year, dt.now().month, dt.now().day, 8)
     ):
         print("Working Hours")
+        with open(host_temp, "r+") as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + " " + website + "\n")
     else:
         print("Party Hours!!")
     time.sleep(5)
