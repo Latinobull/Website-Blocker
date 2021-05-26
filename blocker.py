6c60e6a913ae79acd8e17b324795180fa9db2c1a
Donnahue George <Donnahuegjr@gmail.com>
2021-05-26 09:42:11 -0400

    adding redirect and blocked websites

--- blocker.py
+++ blocker.py
@@ -1,2 +1,4 @@
 hosts_path_Windows = "C:\Windows\System32\drivers\etc\hosts"
 host_path_Mac = "/etc/hosts"
+redirect = "127.0.0.1"
+website_list = ["www.facebook.com", "facebook.com", "www.twitter.com", "twitter.com"]
